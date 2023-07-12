df8cfb88d09de16cbc72cf589f9add2cbcf36d3e
Robert McElroy <robmcelroy145@gmail.com>
2023-07-11 23:35:28 -0500

    processData unfinished, errors occuring at line 62

---
+++ processData.R
@@ -0,0 +1,101 @@
+## Library calls, load gene lists and cosmic signatures ##----------------------
+library(BSgenome.Hsapiens.UCSC.hg19)
+library(deconstructSigs)
+library(data.table)
+source("C:/Users/robmc/Desktop/NUMB/repairGenes.R") # Load repair genes as list objects
+load("C:/Users/robmc/Desktop/NUMB_files/data/signatures.genome.cosmic.v3.may2019.rda")
+'%notin%' <- Negate('%in%')
+homeDir <- "C:/Users/robmc/Desktop/NUMB_files" # Create root data directory pointer 
+
+## processData function ##----------------------
+processData <- function(directory) {
+  setwd(paste0(homeDir, "/data/", directory))
+  setwd(paste0(homeDir,"/data/skcm_tcga"))
+  
+  # Create initial clinical file by merging patient and sample data
+  sample <- fread("data_clinical_sample.txt", skip=4)
+  patient <- fread("data_clinical_patient.txt", skip=4)
+  clin <- merge(sample, patient, by = "PATIENT_ID")
+  # Remove clinical features which only contain NA values
+  clin[clin == "[Not Available]"] <- NA
+  clin <- Filter(function(x)!all(is.na(x)), clin)
+  clin <- clin[clin$SAMPLE_ID %in% clinFilt$SAMPLE_ID, ]
+  
+  # Read in mutation data, accounting for both formats in cBioPortal
+  if (!file.exists("data_mutations.txt")) { mutations <- fread("data_mutations_extended.txt") 
+  } else { mutations <- fread("data_mutations.txt")}
+  
+  # Format chromosome column to match BSgenome format
+  mutations$Chromosome <- sub("^", "chr", mutations$Chromosome)
+  # Prepare input object for deconstructSigs package
+  sigs <- mut.to.sigs.input(mut.ref = mutations, sample.id = "Tumor_Sample_Barcode", chr = "Chromosome",
+                            pos = "Start_Position", ref = "Reference_Allele", 
+                            alt = "Tumor_Seq_Allele2", bsg = BSgenome.Hsapiens.UCSC.hg19)
+  # Filter out samples with low mutation count as suggested by deconstructSigs package
+  # This number can be changed, but further reading should be done to choose mutation cutoff
+  finalMut <- sigs[rowSums(sigs)>50,]
+  results <- vector("list", nrow(finalMut))
+  names(results) <- row.names(finalMut)
+  
+  # Construct mutational profile for each tumor sample from already defined signatures (COSMIC in this case)
+  for(sID in row.names(finalMut)){
+    results[[sID]] <- whichSignatures(finalMut, # the matrix generated with mut.to.sigs.input
+                                      sample.id=sID, # the current sample ID
+                                      signatures.ref=signatures.genome.cosmic.v3.may2019, # the data.frame with the signatures that comes with deconstructSigs
+                                      tri.counts.method="default", # which normalization method to use
+                                      contexts.needed=TRUE) # set to TRUE if your input matrix contains counts instead of frequencies
+  }
+  # convert the exposures for each sample into a sample x signatures matrix
+  expo <- do.call("rbind", sapply(results, "[", 1))
+  # add the unknown value to the matrix such that the contributions add up to 1 per sample
+  Signature.unknown <- unlist(sapply(results, "[", 5))
+  expo <- cbind(expo, Signature.unknown)
+  # Create combined UV exposure signature column
+  expo$UV_sig <- rowSums(expo[ , c("SBS7a", "SBS7b", "SBS7c", "SBS7d")])
+  expo$SAMPLE_ID <- rownames(expo)
+  expo <- expo[order(-expo$UV_sig),]
+  expo$SAMPLE_ID <- gsub(".weights","",expo$SAMPLE_ID)
+  # Output imputed signatures with COSMIC signature names for downstream analysis / plot generation
+  write.csv(expo, "mutationalSignatures.csv")
+  
+  # Create new UV signature annotation column based on % of mutational signature contribution by UV exposure
+  #### SOMETHING IS BREAKING HERE ############
+  clinFinal <- clin[which(clin$SAMPLE_ID %in% expo$SAMPLE_ID), ]
+  clinFinal$UV_sig_value <- expo$UV_sig
+  clinFinal$UV_sig[clinFinal$UV_sig_value >= 0.65] <- "High"
+  clinFinal$UV_sig[clinFinal$UV_sig_value < 0.65 &
+                     clinFinal$UV_sig_value > 0.2] <- "Moderate"
+  clinFinal$UV_sig[clinFinal$UV_sig_value <= 0.2] <- "Low"
+  clinFinal <- clinFinal[order(-clinFinal$UV_sig_value),]
+  # Create tumor_sample_barcode column which is necessary for some maftools package functions
+  clinFinal$Tumor_Sample_Barcode <- clinFinal$SAMPLE_ID
+  
+  mutations$Hugo_Symbol[mutations$Hugo_Symbol == "CUL4A" | mutations$Hugo_Symbol == "CUL4B"] <- "CUL4A/B"  # Combine CUL4A and CUL4B for downstream analysis
+  mutations <- mutations[mutations$Hugo_Symbol %in% NERgenes, ] # Isolate NER mutations
+  mutations <- mutations[mutations$Variant_Classification != "Silent", ] # Filter out silent mutations
+  
+  # Parse through each sample with available UV signature data, and note mutation status of each NER gene (0 = WT, 1 = Mut)
+  clinMut <- as.data.frame(clinFinal$Tumor_Sample_Barcode)
+  for (i in NER) {
+    mut <- mutations[mutations$Hugo_Symbol == i]
+    clinMut[[i]] <- 0
+    clinMut[[i]][clinMut$Tumor_Sample_Barcode %in% mut$Tumor_Sample_Barcode] <- 1
+  }
+  write.csv(clinMut, "mutSig.csv", row.names = F)
+  
+  ############ ENDED HERE FOR THE NIGHT, BELOW NEEDS TO BE CHECKED AND COMMENTED ######################
+  cnv <- fread("data_cna.txt")
+  cnv <- unique(cnv[!duplicated(cnv$Hugo_Symbol), ])
+  cnv2 <- reshape2::melt(cnv, id.vars = "Hugo_Symbol")
+  colnames(cnv2) <- c("Gene", "Sample_name", "CN")
+  cnv2 <- cnv2[cnv2$Sample_name %in% clinFinal$Tumor_Sample_Barcode, ]
+  cnv2$Gene[cnv2$Gene == "CUL4A" | cnv2$Gene == "CUL4B"] <- "CUL4A/B"
+  
+  clinCNV <- clinFinal
+  for (i in NER) {
+    cnv <- cnv2[cnv2$Gene == i, ]
+    clinCNV[[i]] <- 0
+    clinCNV[[i]][match(cnv$Sample_name, clinCNV$Tumor_Sample_Barcode)] <- cnv$CN
+  }
+  write.csv(clinCNV, "cnvSig.csv", row.names = F)
+}
---
+++ repairGenes.R
@@ -0,0 +1,17 @@
+# These genes are often referenced as driver genes in the context of melanoma
+melControl <- c("BRAF", "NRAS", "NF1", "CDKN2A", "PTEN")
+
+# All lists below contian only genes unique to each respective pathway
+NERgenes <- c("CCNH", "CDK7", "CETN2", "CUL4A/B", "DDB1", "DDB2", "ERCC1", "ERCC2",
+         "ERCC3", "ERCC4", "ERCC5", "ERCC6", "ERCC8", "GTF2H1", "GTF2H2", "GTF2H3", "GTF2H4", "GTF2H5",
+         "RAD23A", "RAD23B", "RBX1", "XPA", "XPC", "UVRAG", "UVSSA")
+
+MMRgenes <- c("EXO1", "MLH1", "MLH3", "MSH2", "MSH3", "MSH6", "PCNA", "PMS2")
+
+HRgenes <- c("BLM", "BRCA2", "EME1", "MRE11", "MUS81", "NBN", "RAD50",
+        "RAD51", "RAD51B", "RAD51C", "RAD51D", "RAD52", "RAD54B", "RAD54L", "SEM1",
+        "SSBP1", "TOP3A", "TOP3B", "XRCC2", "XRCC3")
+
+BERgenes <- c("APEX1", "APEX2", "FEN1", "HMGB1", "HMGB1P1", "HMGB1P40", "LIG3", "MBD4", "MPG",
+         "MUTYH", "NEIL1", "NEIL2", "NEIL3", "NTHL1", "OGG1", "PARP1", "PARP2", "PARP3", "PARP4",
+         "POLB", "POLL", "SMUG1", "TDG", "UNG", "XRCC1")
